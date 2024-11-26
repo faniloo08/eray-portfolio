594bc3542b503c093dac2572527c87d7ec18f26f
Fanilo Rabemanantsoa <93961550+faniloo08@users.noreply.github.com>
2024-11-26 19:09:50 +0300

    Add files via upload

---
+++ eray/src/constants/socials.tsx
@@ -0,0 +1,23 @@
+import {
+  IconBrandLinkedin,
+  IconBrandTwitter,
+  IconBrandYoutube,
+} from "@tabler/icons-react";
+
+export const socials = [
+  {
+    href: "https://twitter.com/",
+    label: "Twitter",
+    icon: IconBrandTwitter,
+  },
+  {
+    href: "https://linkedin.com/company/eray-digital",
+    label: "LinkedIn",
+    icon: IconBrandLinkedin,
+  },
+  {
+    href: "https://youtube.com/",
+    label: "YouTube",
+    icon: IconBrandYoutube,
+  },
+];
